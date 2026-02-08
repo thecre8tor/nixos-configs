4817f0d3d4b8d49978fd0ae670281c81c08ff081
Alexander Nitiola <cre8tor.alexander@gmail.com>
2026-02-08 15:47:27 +0100

    fix: update to nixos-unstable and fix Flatpak services

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "Alexander's NixOS Configuration";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-25.05";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
