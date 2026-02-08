79209f113e4bca71d90ad3be9dcd5b585324fa5b
Alexander Nitiola <cre8tor.alexander@gmail.com>
2026-02-08 16:37:20 +0100

    activated the fingerprint module

--- modules/system/fingerprint.nix
+++ modules/system/fingerprint.nix
@@ -1,13 +1,13 @@
-# Audio configuration
+# Fingerprint configuration
 { config, lib, pkgs, ... }:
 
 {
+  services.fprintd.enable = true;
+  
   security.pam.services = {
     login.fprintAuth = true;
-
     # For sudo authentication
     sudo.fprintAuth = true;
-
     # For display manager (if using GDM, SDDM, etc.)
     gdm.fprintAuth = true;  # or sddm, lightdm, etc.
   };
